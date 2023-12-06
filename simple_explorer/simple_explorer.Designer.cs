cf48a79426e4e53f99c5f8dc05736be05957e92a
dEN5 <54153548+dEN5-tech@users.noreply.github.com>
2023-12-06 12:34:59 +0300

    Add files via upload

---
+++ simple_explorer/simple_explorer.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+using System.Windows.Forms;
+
+namespace simple_explorer
+{
+    partial class simple_explorer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private TextBox pathEntry;
+        private Button browseButton;
+        private Button prevButton;
+        private ListBox listBox;
+        private VScrollBar scrollbar;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // Form settings
+            Text = "Directory Viewer";
+            Width = 600;
+            Height = 400;
+
+            // Entry widget for directory path input
+            pathEntry = new TextBox
+            {
+                Width = 400,
+                Location = new System.Drawing.Point(10, 10),
+            };
+            Controls.Add(pathEntry);
+
+            // Button to browse for a directory
+            browseButton = new Button
+            {
+                Text = "Browse",
+                Location = new System.Drawing.Point(420, 10),
+            };
+
+            browseButton.Click += BrowseDirectory;
+            Controls.Add(browseButton);
+
+            // Button for going to the previous directory
+            prevButton = new Button
+            {
+                Text = "<",
+                Location = new System.Drawing.Point(530, 10),
+            };
+            prevButton.Click += OnPrevButtonClick;
+            Controls.Add(prevButton);
+
+            // Listbox to display directory contents
+            listBox = new ListBox
+            {
+                Width = 570,
+                Height = 250,
+                Location = new System.Drawing.Point(10, 40),
+            };
+            listBox.DoubleClick += OnListboxDoubleClick;
+            Controls.Add(listBox);
+
+            // Scrollbar for the Listbox
+            scrollbar = new VScrollBar
+            {
+                Location = new System.Drawing.Point(580, 40),
+                Height = 250,
+            };
+            scrollbar.Scroll += (sender, e) => listBox.TopIndex = ((VScrollBar)sender).Value;
+            Controls.Add(scrollbar);
+        }
+        #endregion
+
+
+    }
+}
+
---
+++ simple_explorer/simple_explorer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace simple_explorer
+{
+    public partial class simple_explorer : Form
+    {
+
+        public simple_explorer()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void BrowseDirectory(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string directoryPath = folderDialog.SelectedPath;
+                    pathEntry.Text = directoryPath;
+                    ShowDirectoryContents(directoryPath);
+                }
+            }
+        }
+
+        private void OnListboxDoubleClick(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox.SelectedIndex;
+            if (selectedIndex >= 0)
+            {
+                string selectedDirectory = listBox.Items[selectedIndex].ToString();
+                string currentPath = pathEntry.Text;
+                string newPath = Path.Combine(currentPath, selectedDirectory);
+                pathEntry.Text = newPath;
+                ShowDirectoryContents(newPath);
+            }
+        }
+
+        private void ShowDirectoryContents(string path)
+        {
+            listBox.Items.Clear(); // Clear existing contents
+
+            try
+            {
+                string[] files = Directory.GetFiles(path);
+                string[] directories = Directory.GetDirectories(path);
+
+                foreach (string directory in directories)
+                {
+                    listBox.Items.Add(Path.GetFileName(directory));
+                }
+
+                foreach (string file in files)
+                {
+                    listBox.Items.Add(Path.GetFileName(file));
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox.Items.Add($"Error: {ex.Message}");
+            }
+        }
+
+        private void OnPrevButtonClick(object sender, EventArgs e)
+        {
+            string currentPath = pathEntry.Text;
+
+            // Get the parent directory
+            string parentDirectory = Directory.GetParent(currentPath)?.FullName;
+
+            if (!string.IsNullOrEmpty(parentDirectory))
+            {
+                pathEntry.Text = parentDirectory;
+                ShowDirectoryContents(parentDirectory);
+            }
+        }
+    }
+}
